18a4f7dca2bbd3e218e5a657e8fb43a91d0c06e5
Sabuj Jana <sabujlovescoding@gmail.com>
2020-10-15 00:44:44 +0530

    subedu.js added

---
+++ src/sections/Education/SampleModal.js
@@ -0,0 +1,91 @@
+/* eslint react/no-multi-comp: 0, react/prop-types: 0 */
+
+import React, { useState, useEffect } from "react";
+
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { connect } from "react-redux";
+import { updateAbout } from "../../components/actions/taskActions";
+
+const ModalExample = (props) => {
+  const { buttonLabel, className, data, updateAbout } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+  const [fullnameModal, setFullname] = useState("");
+  const [summaryModal, setSummary] = useState("");
+
+//   useEffect(() => {
+//     console.log(data);
+//     setFullname(data[0].fullname);
+//     setSummary(data[0].summary);
+//   }, [data]);
+
+  const handleChange = (e) => {
+    switch (e.target.id) {
+      case "fullnameModal":
+        setFullname(e.target.value);
+        return;
+      case "summaryModal":
+        setSummary(e.target.value);
+        return;
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log({ data, fullname: fullnameModal, summary: summaryModal });
+    // updateAbout({ data, fullname: fullnameModal, summary: summaryModal });
+    toggle();
+  };
+
+  return (
+    <div>
+      <span
+        className="material-icons text-warning"
+        style={{ cursor: "pointer" }}
+        onClick={toggle}
+      >
+        create
+      </span>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>
+          <input
+            type="text"
+            className="form-control"
+            id="fullnameModal"
+            value={fullnameModal}
+            onChange={handleChange}
+          />
+        </ModalHeader>
+        <ModalBody>
+          <textarea
+            className="form-control"
+            id="summaryModal"
+            rows="4"
+            value={summaryModal}
+            onChange={handleChange}
+          ></textarea>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={handleSubmit}>
+            Update
+          </Button>{" "}
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateAbout: (task) => dispatch(updateAbout(task)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(ModalExample);
--- src/sections/Education/SubEdu.js
+++ src/sections/Education/SubEdu.js
@@ -1,9 +1,11 @@
 import React from "react";
+import SampleModal from "./SampleModal"
 
 const SubEdu = ({ data }) => {
   return (
-    <div className="card">
-      <div className="card-header bg-dark text-white">Your Data</div>
+    <div className="card">       
+        <div className="card-header bg-dark text-white">Your Data <SampleModal /></div> 
+                       
       <div className="card-body">
         <h5 className="card-title">{data.instiname}</h5>
         <p className="card-text">{data.location}</p>
