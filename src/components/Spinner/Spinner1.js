cf64a9573cc7230d5a338235d51f6c4e32702000
Sabuj Jana <sabujlovescoding@gmail.com>
2020-10-14 20:41:29 +0530

    Spinner added

---
+++ src/components/Spinner/Spinner1.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spinner1 = () => {
+  return (
+    <div className="spinner-grow text-warning" role="status">
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+};
+
+export default Spinner1;
